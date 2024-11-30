50bab7801f981705a506427a5c9314dbe648ca0c
heinstein <3276604+heinsteinh@users.noreply.github.com>
2024-12-01 00:32:42 +0100

    Working project with variable group set and working for creating real infra in azure cloud

--- infra/main.tf
+++ infra/main.tf
@@ -1,4 +1,18 @@
-resource "azuredevops_project" "project" {
-  name        = "Project Name"
-  description = "Project Description"
+resource "azuredevops_project" "main" {
+  name        = "Agile-Project-Tf"
+  description = "Simple project deploy with terraform to quickly and easily create new Azure DevOps projects."
+
+  visibility         = "private"
+  version_control    = "Git"
+  work_item_template = "Agile"
+}
+
+resource "azuredevops_git_repository" "infra" {
+  project_id = azuredevops_project.main.id
+  name       = "infrastructure"
+  initialization {
+    init_type   = "Import"
+    source_type = "Git"
+    source_url  = "https://github.com/fkheinstein204/azdo-tf-tpl-multi-stage.git"
+  }
 }
